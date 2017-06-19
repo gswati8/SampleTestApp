fecd6bce90ee3149a5248f29ae587f802a1dcdbb
swatigarg <swati.garg@adcolony.com>
2017-06-19 11:47:06 -0700

    Initial Commit

---
+++ TestApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApplication
+//
+//  Created by Swati on 6/19/17.
+//  Copyright © 2017 adcolony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestApplication/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestApplication
+//
+//  Created by Swati on 6/19/17.
+//  Copyright © 2017 adcolony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
